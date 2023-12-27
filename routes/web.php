adb7abf9ec99c8078175fcb8b5ad4f3365a01e44
huy <huy@gmail.com>
2023-12-27 23:17:36 +0700

    uncomment router

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('home');
-//});
+Route::get('/', function () {
+    return view('home');
+});
 Route::group(['namespace' => 'App\Http\Controllers'], function()
 {
     /**
